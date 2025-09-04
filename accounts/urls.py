1f085d26172a2fd490da52a273898a086dc954c4
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-04 09:51:14 +0330

    add urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from .views import RegisterView, LoginView, task_list
+from .views import RegisterView
 from django.urls import path
- 
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('task_list', task_list, name='task_list')
+    path('login/', LoginView.as_view(next_page='task_list'), name='login'),
+    path('logout/', LogoutView.as_view(next_page = 'register'), name='logout'),
 ]
