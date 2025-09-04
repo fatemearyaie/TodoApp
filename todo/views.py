ddcd9f8ded8d10d8fa28150ffd413eacf56714ef
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-04 10:00:45 +0330

    write listView class in todo/views.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Task
 # Create your views here.
+class Task_list(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'task_list.html'
