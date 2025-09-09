f201635a83de7b198fc59f5896870cf990eee5c8
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-09 07:55:19 +0330

    update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,18 +3,26 @@ from django.contrib.auth.views import LoginView
 from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.contrib.auth import login, logout
-from django.http import HttpResponse
+from django.contrib.auth import login
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = "registration/login.html"
+    fields = "username","password"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("todo:tasklist")
+    
 class RegisterView(FormView):
     """
     it shows which template this view is related, which form class is related and what should we do after the success
     """
-    template_name = 'registeration/register.html'
+    template_name = 'registration/register.html'
     form_class = UserCreationForm
     redirect_authenticate_user = True
-    success_url = reverse_lazy('task_list')
+    success_url = reverse_lazy('accounts:login')
     """
     get a form, save it. if the form is not none login the user.
     """
@@ -25,5 +33,6 @@ class RegisterView(FormView):
         return super(RegisterView, self).form_valid(form)
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirect('login')
+            return redirect('todo:tasklist')
         return super(RegisterView, self).get(*args, **kwargs)
+    
