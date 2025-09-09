356854a5859e5904ae50ac3be48c4a7d0bfd44c0
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-09 07:53:00 +0330

    update models.py

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, blank=True, null=True)
     id = models.CharField(max_length=4, primary_key=True)
     task_name = models.CharField(max_length=50)
     complete = models.BooleanField(default=False)
