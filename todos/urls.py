2a266c1accf0086e80cce35fcf18371018c76b3d
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-19 12:53:01 +0330

    update urls to have CUD either

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ListTodo, DetailTodo
+from .views import ListTodo, DetailTodo, CreateTodo, UpdateTodo, DeleteTodo
+
 urlpatterns = [
     path('', ListTodo.as_view()),
     path('<int:pk>', DetailTodo.as_view()),
+    path('<int:pk>/delete', DeleteTodo.as_view()),
+    path('create', CreateTodo.as_view()),
+    path('<int:pk>/update', UpdateTodo.as_view()),
 ]
