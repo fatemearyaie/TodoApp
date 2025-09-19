8decc0b1158df87a0f0a222b5d3769df20143e87
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-19 12:53:30 +0330

    write views to have CUD either

--- todos/views.py
+++ todos/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import TodoSerializer
 from .models import Todo
+
+
 # Create your views here.
 class ListTodo(generics.ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class CreateTodo(generics.CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DeleteTodo(generics.DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class UpdateTodo(generics.UpdateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
